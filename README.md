875211474ba11a3a8e4ca8a6ec339879a18ed0d0
Kodest <real.kodest@gmail.com>
2025-08-17 02:22:47 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# KMK_tools
+# KMK tools
+
 KMK tools helps with making loops and joining text
+
+## Installation
+
+You can install `KMK_tools` using `pip`:
+
+```bash
+pip install KMK_tools
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='KMK_tools',
+    version='0.1.0',
+    author='Kodest',
+    author_email='real.kodest@gmail.com',
+    description='KMK tools helps with making loops and joining text',
+    url='https://github.com/real-kodest/KMK_tools/',
+    packages=find_packages(),
+    install_requires=[],
+)
